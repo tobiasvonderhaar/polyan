f4897b2b45ba1431a45318330bb2924f7ad27426
Tobias von der Haar <T.von-der-Haar@kent.ac.uk>
2020-06-24 20:57:00 +0100

    Corrected directory structure

--- polyan/setup.py
+++ polyan/setup.py
@@ -4,19 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="polyan-tobiasvonderhaar",
+    name="polyan",
     version="2020.1",
     author="Tobias von der Haar",
     author_email="T.von-der-Haar@kent.ac.uk",
+    license = "MIT",
     description="A package for simulating polysome profiles from Ribo-Seq data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/tobiasvonderhaar/polyan",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Framework :: Jupyter",
+        "Framework :: Matplotlib",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Visualization",
     ],
     python_requires='>=3.6',
 )
